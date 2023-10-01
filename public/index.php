8ba84ff7051299cd123ac495f5a005fad867310e
manny <mhortizuela@dmmmsu.edu.ph>
2023-10-01 16:25:06 +0800

    upload api

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+require '../src/vendor/autoload.php';
+	$app = new \Slim\App;
+	//endpoint get greeting
+	$app->get('/getName/{fname}/{lname}', function (Request $request, Response
+	$response, array $args) {
+		$info=	array("fname"=>$args['fname'],"lname"=>$args['lname']);
+		
+		return json_encode($info);
+	});
+	//endpoint post greeting
+	$app->post('/postName', function (Request $request, Response $response, array $args)
+	{
+		$data=json_decode($request->getBody());
+		$info=array();
+		array_push($info,array("lname"=>$data->lastname, 
+		"fname"=>$data->firstname,"dept"=>$data->department,
+		"col"=>$data->college));
+		return $response->getBody()->write(json_encode(array(
+			"status"=>"sucess",
+		"data"=>$info)));
+	});
+	$app->run();
+?>
